a85210b45a527f8633d43afc4d3de6e127f52bd3
Никита Сергеевич Хохлов <525.khokhlovns@cit.cs>
2023-05-18 10:33:20 +0300

    Удалён мусор

--- src/FluentHTTPToolkit/FluentHTTPTollkit.Client.Console/DTOs/LoginRequestDTO.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace FluentHTTPTollkit.Client.Console.DTOs
-{
-    internal class LoginRequestDTO
-    {
-        public string Username { get; set; }
-        public string Password { get; set; }
-    }
-}
